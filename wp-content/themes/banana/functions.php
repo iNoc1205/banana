f27ebc5d8128c3b0b81a034f6d3496701692928b
iNoc1205 <enoc.villa16@gmail.com>
2020-10-21 17:33:29 -0600

    Extras y Acabados

--- wp-content/themes/banana/functions.php
+++ wp-content/themes/banana/functions.php
@@ -91,10 +91,10 @@ function acf_load_material_impresion_field_choices( $field ) {
 }
 add_filter('acf/load_field/name=elegir_material_impresion', 'acf_load_material_impresion_field_choices'); 
 
-/* $postId = $_GET['post'];
+$postId = $_GET['post'];
 $test = get_field('anadir_material' , $postId);
-$rows = have_rows('anadir_material' , $postId);
-var_dump($test); */
+// $rows = have_rows('anadir_material' , $postId);
+var_dump($test);
 
 function acf_load_saved_tipo_taller($field){
 	remove_filter( current_filter(), __FUNCTION__ );
@@ -166,6 +166,41 @@ function acf_load_saved_tipo_imprsion($field){
 }
 add_filter('acf/load_field/name=elegir_tipo_impresion', 'acf_load_saved_tipo_imprsion');
 
+function acf_load_saved_acabado($field){
+	remove_filter( current_filter(), __FUNCTION__ );
+
+	$postId = '';
+
+	if(isset($_GET['post'])) {
+		$postId = $_GET['post'];
+	}
+	
+	$field['choices'] = array();
+	
+	$rows = get_field('anadir_material' , $postId);
+
+	if( $rows ) {
+		foreach( $rows as $row ) {
+			if($row['acabados']){
+				$material = $row['elegir_material_impresion'];
+
+				$arrayName = array('body' => array('productos' => $material ) );
+				$url = 'https://calculadora.imprimebanana.com/api/impresion/acabados';
+				$acabados = wp_remote_post( $url , $arrayName  );
+				$bodyAcabado = wp_remote_retrieve_body($acabados);
+				$dataAcabado = json_decode($bodyAcabado, true);
+
+				$acabado = $row['acabados'];
+				$nombre = $dataAcabado[$acabado];
+				$field['choices'][ $acabado ] = $nombre;
+			}
+		}
+	}
+
+    return $field;
+}
+add_filter('acf/load_field/name=acabados', 'acf_load_saved_acabado');
+
 ///////////////////////////// ANDREY //////////////////////////////////
 
 
@@ -177,16 +212,8 @@ function asv_insertar_js(){
 
     wp_enqueue_media();
     wp_register_script('asv_script', get_stylesheet_directory_uri(). '/script.js', array('jquery'), '1', true );
-    wp_enqueue_script('asv_script');
-
-    /*wp_localize_script( 'my_js', 'ajax_var', array(
-        'url'    => admin_url( 'admin-ajax.php' ),
-        'nonce'  => wp_create_nonce( 'my-ajax-nonce' ),
-        'action' => 'event-list'
-    ) );*/
+	wp_enqueue_script('asv_script');
 }
-
-
 
 function fn_obtener_tipos_taller() {
 
@@ -198,10 +225,22 @@ function fn_obtener_tipos_taller() {
         $datos = $data["taller"]["tipos"][ $_POST['id_producto'] ];
     }
 
-    $retornar = '';
+	$retornar = '';
 
- 
+	/* if(isset($_GET['post'])) {
+		$postId = $_GET['post'];
+	}
+	$rows = get_field('anadir_material' , $postId);
+
+	$desgloseId = explode('row-', $_POST['idSelect']);
+	$nRow = $desgloseId[1][0];
+	$idGuardado = $rows[$nRow]['elegir_tipo']; */
+
     foreach ($datos as $key => $value) {
+		/* $activo = '';
+		if ($idGuardado == $key){
+			$activo = 'selected';
+		} */
         $retornar .= '<option value="'.$key.'">'.$value.'</option>';
 	}    
 
@@ -209,34 +248,74 @@ function fn_obtener_tipos_taller() {
 
     wp_die();
 }
-add_action( 'wp_ajax_obtener_tipos_taller', 'fn_obtener_tipos_taller' );
+add_action('wp_ajax_obtener_tipos_taller', 'fn_obtener_tipos_taller');
+
+function fn_obtener_extras_taller() {
+
+	$arrayName = array('body' => array('tipos' => $_POST['id_producto'], 'troquel' => 1, 'impresion' => 0) );
+	$url = 'https://calculadora.imprimebanana.com/api/taller/extras';
+	$extras = wp_remote_post( $url , $arrayName  );
+	$body = wp_remote_retrieve_body($extras);
+	$dataExtras = json_decode($body, true);
+
+	$desgloseId = explode('row-', $_POST['id_select']);
+	$nRow = $desgloseId[1][0];
+
+	$retornar = '';
+
+    foreach ($dataExtras as $key => $value) {
+		if( $value == 'error'|| $value == 'No existen extras para está configuración'){
+			$retornar = '<p>No hay opciones disponibles</p>';
+			break;
+		}
+		$retornar .= '<li><label><input type="checkbox" name="acf[field_5f891556cbe87][row-'.$nRow.'][field_5f8fa8bdd4c85][]" value="'.$key.'" >'.$value.'</label></li>';
+	}    
+	// var_dump($retornar);
+    echo $retornar;
+    wp_die();
+}
+add_action('wp_ajax_obtener_extras_taller', 'fn_obtener_extras_taller');
 
 function fn_obtener_tipos_impresion() {
 
+	//GET PRODUCTOS
     $request = wp_remote_get('https://calculadora.imprimebanana.com/api/productos');
     if( ! empty( $request ) ) {
         $body = wp_remote_retrieve_body($request);
         $data = json_decode($body, true);
         //var_dump($data);
 		$datos = $data["impresion"]["tipos"][ $_POST['id_iproducto'] ];
-    }
+	}
+	
+	//POST ACABADOS
+	$arrayName = array('body' => array('productos' => $_POST['id_iproducto']) );
+	$url = 'https://calculadora.imprimebanana.com/api/impresion/acabados';
+	$acabados = wp_remote_post( $url , $arrayName  );
+	$bodyAcabado = wp_remote_retrieve_body($acabados);
+	$dataAcabado = json_decode($bodyAcabado, true);
 
-    $retornar = '';
+	$retornar = array();
+	
+	$tipo = '';
+	$acabado='';
 
     foreach ($datos as $key => $value) {
-        $retornar .= '<option value="'.$key.'">'.$value["nombre"].'</option>';
+        $tipo .= '<option value="'.$key.'">'.$value["nombre"].'</option>';
     }    
-    
-
-    echo $retornar;
+	
+	foreach ($dataAcabado as $key => $value) {
+        $acabado .= '<option value="'.$key.'">'.$value.'</option>';
+	}
+	
+	$retornar [0] = $tipo;
+	$retornar [1] = $acabado;
+    echo json_encode($retornar);
 
     wp_die();
 }
 add_action( 'wp_ajax_obtener_tipos_impresion', 'fn_obtener_tipos_impresion' );
 
 ///////////////////////////////////////////////////////////////////////////
-
-
 
 function create_quantity_taxonomy() {
 	$labels = array(
@@ -260,24 +339,41 @@ function create_quantity_taxonomy() {
 
 add_action( 'init', 'create_quantity_taxonomy');
 
-function create_dimensions_taxonomy() {
-	$labels = array(
-		'name'                           => 'Dimensiones',
-		'singular_name'                  => 'Dimensión',
-		'add_new_item'                   => 'Añadir una dimensión',
-		'new_item_name'                  => 'Nueva dimensión',
-		'menu_name'                      => 'Dimensiones',
-		'add_or_remove_items'            => 'Añadir o Borrar Dimensiones'
-	);
-	register_taxonomy(
-		'dimension',
-		'post',
-		array(
-			'label' => __( 'Dimensión' ),
-			'hierarchical' => false,
-			'labels' => $labels
-		)
-	);
-}
+// Register Custom Taxonomy
+function custom_taxonomy_dimension() {
 
-add_action( 'init', 'create_dimensions_taxonomy' );
+	$labels = array(
+		'name'                       => _x( 'Dimensiones', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Dimensión', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Dimensiones', 'text_domain' ),
+		'all_items'                  => __( 'Todas las Dimensiones', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'taxonomy', array( 'post' ), $args );
+
+}
+add_action( 'init', 'custom_taxonomy_dimension', 0 );
--- wp-content/themes/banana/script.js
+++ wp-content/themes/banana/script.js
@@ -9,6 +9,8 @@
    // this might need to target the hidden field, not sure, testing would be required
    // it really depends on the specifics of the field settings
    // you need to inspect the HTML see the structure of the field
+
+   ///////////////////// TIPOS TALLER /////////////////////
    $(document).on('change', '[data-key="field_5f8915adcbe89"] .acf-input select', function(e) {
      
      // once your getting the change event to fire
@@ -31,6 +33,29 @@
      });     
    });
 
+   ///////////////////// TALLER EXTRAS /////////////////////
+  $(document).on('change', '[data-key="field_5f8a0db7c4bc2"] .acf-input select', function(e) {
+
+    var valor = this.value;
+
+    var selectExtras = jQuery(this).parents('.acf-row').find('[data-key="field_5f8fa8bdd4c85"] .acf-input ul');
+    var idSelect = jQuery(this).attr('id');
+    console.log(idSelect);
+    selectExtras.html('<p>Cargando...</p>');
+
+    jQuery.ajax({
+        type: "post",
+        url: ajaxurl,
+        data: "action=obtener_extras_taller&id_producto=" + valor +"&id_select="+idSelect,
+        success: function(result){
+           selectExtras.empty();
+           selectExtras.html(result);
+        }
+    });     
+  });
+
+
+  ///////////////////// TIPOS IMPRESION /////////////////////
   $(document).on('change', '[data-key="field_5f8915e7cbe8a"] .acf-input select', function(e) {
      
     // once your getting the change event to fire
@@ -39,21 +64,60 @@
     var valor = this.value;
     //field.hide(); -  field.show();
 
-     var selectTipo = jQuery(this).parents('.acf-row').find('[data-key="field_5f8a6e9f2341f"] .acf-input select');
-     selectTipo.html('<option>Cargando...</option>');
+    var selectTipo = jQuery(this).parents('.acf-row').find('[data-key="field_5f8a6e9f2341f"] .acf-input select');
+    selectTipo.html('<option>Cargando...</option>');
+
+    var selectAcabado = jQuery(this).parents('.acf-row').find('[data-key="field_5f8ddfac8324c"] .acf-input select');
+    selectAcabado.html('<option>Cargando...</option>');
+
+    
 
     jQuery.ajax({
         type: "post",
         url: ajaxurl,
         data: "action=obtener_tipos_impresion&id_iproducto=" + valor,
         success: function(result){
-           selectTipo.empty();
-           selectTipo.html(result);
+          var json = JSON.parse(result);
+          selectTipo.empty();
+          selectAcabado.empty();
+          selectTipo.html(json[0]);
+          selectAcabado.html(json[1]);
         }
     });
-
 
     // https://www.advancedcustomfields.com/resources/javascript-api/
     
   });
- })(jQuery);
+ 
+  /* $(document).on('change', '[data-key="field_5f8915adcbe89"] .acf-input select', function(e) {
+    cargarTipos(jQuery(this));
+  });
+
+  $(document).ready(function(){
+    let rows = jQuery('.acf-field-5f891556cbe87 .acf-table .acf-row');
+    jQuery.each(rows, function(i, val) {
+      let selectTaller = jQuery(this).find('[data-name="elegir_meterial_taller"] select');
+      cargarTipos(selectTaller);
+    });
+  });
+
+
+  function cargarTipos($this)
+  {
+    var valor = $this.val();
+    var selectTipo = $this.parents('.acf-row').find('[data-key="field_5f8a0db7c4bc2"] .acf-input select');
+    var idSelect = $this.attr('id');
+    console.log(idSelect);
+    selectTipo.html('<option>Cargando...</option>');
+
+    jQuery.ajax({
+      type: "post",
+      url: ajaxurl,
+      data: "action=obtener_tipos_taller&id_producto=" + valor  +"&idSelect=" +idSelect,
+      success: function(result){
+        selectTipo.empty();
+        selectTipo.html(result);
+      }
+    });
+  } */
+})(jQuery);
